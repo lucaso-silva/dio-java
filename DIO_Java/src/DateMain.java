6fb9c254bfbe2c6561031661c3ce1128348ce25d
Lucas Oliveira <luc.osilva@gmail.com>
2025-09-02 23:54:54 -0700

    Classes LocalDate, LocalTime, LocalDateTime

--- DIO_Java/src/DateMain.java
+++ DIO_Java/src/DateMain.java
@@ -1,5 +1,8 @@
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.Calendar;
 
@@ -28,5 +31,47 @@ public class DateMain {
         System.out.println(formatter.format(date));
         * */
 
+        //LocalDate
+        System.out.println("==== LocalDate ====");
+        LocalDate localDate = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        System.out.println(formatter.format(localDate));
+
+        System.out.println("\nString to date");
+        var strDate = "29/08/2025";
+        System.out.println(LocalDate.parse(strDate, formatter));
+        System.out.println(formatter.format(LocalDate.parse(strDate, formatter)));
+
+        System.out.printf("\n%s\n", localDate.plus(15, ChronoUnit.DAYS));
+        System.out.println(localDate.plusDays(150));
+        System.out.println(localDate.getDayOfWeek());
+        System.out.println(localDate.getMonth());
+
+        //LocalTime
+        System.out.println("==== LocalTime ====");
+        LocalTime localTime = LocalTime.now();
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("hh:mm:ss");
+        DateTimeFormatter formatter3 = DateTimeFormatter.ofPattern("HH:mm:ss");
+        System.out.println(localTime.format(formatter2));
+        System.out.println(localTime.format(formatter3));
+        System.out.println(localTime.getHour());
+
+        //LocalDateTime
+        System.out.println("==== LocalDateTime ====");
+        LocalDateTime localDateTime = LocalDateTime.now();
+        DateTimeFormatter formatter4 = DateTimeFormatter.ISO_DATE_TIME;
+        System.out.println(localDateTime.format(formatter4));
+        System.out.println(localDateTime.toInstant(ZoneOffset.UTC));
+        Date date = Date.from(localDateTime.toInstant(ZoneOffset.ofHours(+3)));
+        System.out.println(date);
+
+        Date date2 = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date2);
+        ZoneId zoneId = calendar.getTimeZone().toZoneId();
+        LocalDateTime localDateTime2 = LocalDateTime.ofInstant(calendar.toInstant(), zoneId);
+        System.out.println(localDateTime2);
+
+        System.out.println(Duration.between(localDateTime, localDateTime2).toMillis());
     }
 }
